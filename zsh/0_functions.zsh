e2c9058d47d5087add6211385f931b36075b1542
Corey Jewett <corey@iota.local>
2015-02-18 12:38:59 -0800

    df_mk_remote

--- zsh/0_functions.zsh
+++ zsh/0_functions.zsh
@@ -7,13 +7,24 @@
 #
 # will fetch the PNG/HTML (or several of each if the <fname> globs them),
 # open the PNG(s), and show URIs for the HTML. If your <fname> is bad it'll tell you.
-jenkins_get() {
+df_jenkins_get() {
   fname=$(echo $1 | sed -Ee 's|^/tmp/||; s/.(png|html)$//;');
   scp jenkins.connect.fluke.com:/tmp/$fname\* /tmp;
-  if [ $? -ne 0 ]; then
+  if [[ $? -ne 0 ]]; then
     echo Invalid asset name: $fname 1>&2 1>/dev/null;
   else
     for x in /tmp/$fname*.html; do echo file://$x; done
     open /tmp/$fname*.png;
   fi
+}
+
+df_mk_remote() {
+  local dry="";
+  if [[ "$1" == "--dry-run" ]]; then
+    dry="echo";
+  fi
+  local personal=personal/cjewett;
+  local current_branch=`git rev-parse --abbrev-ref HEAD`;
+  $dry git push origin $current_branch:$personal/$current_branch
+  $dry git branch -u origin/$personal/$current_branch $current_branch
 }
