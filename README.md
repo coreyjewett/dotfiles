14c4555e578b10e44d5a9b5e3654b54f5d8a2889
Corey Jewett <cj@syntheticplayground.com>
2021-09-01 06:36:52 +0000

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+Borrowing the choicest bits of:
+* [lfilho's fork of YADR](https://github.com/lfilho/dotfiles.git)
+
+And managing them with [chezmoi](https://www.chezmoi.io/).
---
+++ dot_zsh/fzf.zsh
@@ -0,0 +1,22 @@
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
+  export PATH="$PATH:/usr/local/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.zsh"
+
+# fasd & fzf change directory - jump using `fasd` if given argument, filter output of `fasd` using `fzf` else
+unalias z #removing fasd's alias for z first
+
+z() {
+  [ $# -gt 0 ] && fasd_cd -d "$*" && return
+  local dir
+  dir="$(fasd -Rdl "$1" | fzf -1 -0 --no-sort +m)" && cd "${dir}" || return 1
+}
