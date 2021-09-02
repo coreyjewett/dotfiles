4985009ed012ba43f4d1ae507fab89a780ecb4bf
Corey Jewett <corey.jewett@smartsheet.com>
2021-09-02 00:46:17 +0000

    add linuxbrew to PATH if found

--- dot_zsh/0_path.zsh
+++ dot_zsh/0_path.zsh
@@ -1,7 +1,10 @@
 # path, the 0 in the filename causes this to load first
-#
+
+# Try to load linuxbrew if it exists
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+
 # If you have duplicate entries on your PATH, run this command to fix it:
-# PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
+PATH=$(echo "$PATH" | awk -v RS=':' -v ORS=":" '!a[$1]++{if (NR > 1) printf ORS; printf $a[$1]}')
 
 pathAppend() {
   # Only adds to the path if it's not already there
