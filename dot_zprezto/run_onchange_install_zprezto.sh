930ffe925f80937446e24c34d89119abb3be4199
Corey Jewett <corey.jewett@smartsheet.com>
2021-12-28 17:03:46 -0800

    Turn zprezto back on

---
+++ dot_zprezto/run_onchange_install_zprezto.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+ZPREZTO_DIR="${ZDOTDIR:-$HOME}/.zprezto"
+
+echo "Installing/Updating zprezto in $ZPREZTO_DIR..."
+
+(
+  if [ ! -e $ZPREZTO_DIR ]; then
+    git clone --recursive https://github.com/sorin-ionescu/prezto.git $ZPREZTO_DIR
+  else
+    git pull
+  fi
+  
+  cd $ZPREZTO_DIR
+  git submodule sync --recursive
+  git submodule update --init --recursive
+) | sed 's/^/    /';
