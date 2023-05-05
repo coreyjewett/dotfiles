4c13695fbbbb1781b4ab3fdec94a414656a1f3a1
Corey Jewett <corey.jewett@smartsheet.com>
2023-05-05 09:55:55 -0700

    standalone Google apps

---
+++ mk_gmail.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# https://github.com/nativefier/nativefier/blob/master/API.md#url-handling-options
+
+TARGET=~/Applications/nativefier-apps
+
+nativefier() {
+	local uri="$1";
+	local name="$2";
+	shift 2;
+
+	# TODO this doesn't work because there is an intermediate directory created.
+	if [ -e "$TARGET/$name" ]; then
+		echo "TODO upgrade";
+	else
+		npx nativefier "$uri" "$TARGET" --name "$name" "$@";
+	fi
+}
+
+# Google Calendar
+nativefier 'https://mail.google.com/' "GMail" \
+	--internal-urls '.*?mail.google.*?' \
+	--single-instance
