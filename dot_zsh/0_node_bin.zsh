30b7661f374bfba36bf10531f4f52efb4e5ea0be
Corey Jewett <corey.jewett@smartsheet.com>
2024-09-13 17:36:52 -0700

    node_modules/.bin in the PATH

---
+++ dot_zsh/0_node_bin.zsh
@@ -0,0 +1,15 @@
+# https://github.com/remcohaszing/zsh-node-bin/blob/main/node-bin.plugin.zsh
+# Add node_modules/.bin to path.
+function _node_bin() {
+  path=( ${path[@]:#*node_modules*} )
+  local p="$(pwd)"
+  while [[ "$p" != '/' ]]; do
+    if [[ -d "$p/node_modules/.bin" ]]; then
+      path+=("$p/node_modules/.bin")
+    fi
+    p="$(dirname "$p")"
+  done
+  typeset -U path
+}
+
+precmd_functions+=(_node_bin)
