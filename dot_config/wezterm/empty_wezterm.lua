5c69940607377dd442b8f9e02cff64f4ba047424
Corey Jewett <corey.jewett@smartsheet.com>
2024-11-08 16:48:35 -0800

    persist initial WM config

--- dot_config/wezterm/empty_wezterm.lua
+++ dot_config/wezterm/empty_wezterm.lua
@@ -9,5 +9,7 @@ config.harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' }
 
 config.font = wezterm.font("MesloLGS Nerd Font")
 
+config.window_background_opacity = 0.9
+
 -- and finally, return the configuration to wezterm
 return config
