c956f92edbc1ac36ca5423fc6848647d4247a3ef
Corey Jewett <corey.jewett@smartsheet.com>
2025-03-19 11:02:35 -0700

    wezterm: Ask before closing

--- dot_config/wezterm/empty_wezterm.lua
+++ dot_config/wezterm/empty_wezterm.lua
@@ -11,5 +11,17 @@ config.font = wezterm.font("MesloLGS Nerd Font")
 
 config.window_background_opacity = 0.9
 
+-- stop faat-fingering windows closed
+config.keys = {
+  {
+    key = 'w',
+    mods = 'CMD',
+    action = wezterm.action.CloseCurrentTab { confirm = true },
+  },
+}
+config.skip_close_confirmation_for_processes_named = { }
+
+config.front_end = "WebGpu"
+
 -- and finally, return the configuration to wezterm
 return config
